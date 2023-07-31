602319d09ac2e1860af20c200b45b199b244a5bb
edwan <edwan@yandex.ru>
2023-08-01 01:38:54 +0300

    Изменить README добавить новую витрину

---
+++ run_script_QueryMonitoring.cmd
@@ -0,0 +1,11 @@
+
+@ECHO OFF
+
+ECHO Runing SQL script c:\run_SQL_scripts\script_QueryMonitoring.sql
+ECHO See file c:\sql\my_QueryMonitoring.txt
+
+
+
+sqlcmd -S OCTOPUS\SQLEXPRESS -i c:\run_SQL_scripts\script_QueryMonitoring.sql -o c:\sql\my_QueryMonitoring.txt
+
+notepad.exe c:\sql\my_QueryMonitoring.txt
