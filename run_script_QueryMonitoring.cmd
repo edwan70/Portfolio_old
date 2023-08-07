bde0dc057fb3ecab38610f10bf47e27348b34468
edwan <edwan@yandex.ru>
2023-08-07 16:38:11 +0300

    Изменить простую витрину

--- run_script_QueryMonitoring.cmd
+++ run_script_QueryMonitoring.cmd
@@ -6,6 +6,6 @@ ECHO See file c:\sql\my_QueryMonitoring.txt
 
 
 
-sqlcmd -S OCTOPUS\SQLEXPRESS -i c:\run_SQL_scripts\script_QueryMonitoring.sql -o c:\sql\my_QueryMonitoring.txt
+sqlcmd -S OCTOPUS\SQLEXPRESS_2012 -i c:\run_SQL_scripts\script_QueryMonitoring.sql -o c:\sql\my_QueryMonitoring.txt
 
 notepad.exe c:\sql\my_QueryMonitoring.txt
